a4fc4079cfd8954bcbf17357cb0db53d362038b5
polina-Z <polina.zorko@mail.ru>
2020-04-07 16:17:37 +0300

    Create lab5.c

---
+++ lab5/lab5.c
@@ -0,0 +1,114 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define MAX 2147483646
+
+#include <stdlib.h>
+#include <malloc.h>
+#include <stdio.h>
+#include <string.h>
+
+struct ListItem
+{
+	struct Node
+	{
+		int data;
+		struct Node* next;
+		struct Node* prev;
+	};
+	struct Node* head;
+	struct Node* tail;
+};
+
+int VerificationI()
+{
+	int choice = 0;
+	char term1;
+	while (1)
+	{
+		rewind(stdin);
+		if (scanf_s("%d%c", &choice, &term1, 102) != 2 || term1 != '\n' || choice <= 0 || choice > MAX)
+		{
+			printf("Неправильный ввод.\n");
+		}
+		else
+		{
+			return choice;
+		}
+	}
+}
+
+
+void ListItemZ(struct ListItem* list)
+{
+	list->head = NULL;
+	list->tail = NULL;
+}
+
+void AddList(struct ListItem* list, int data)
+{
+	struct Node* item = (struct Node*)malloc(sizeof(struct Node));
+	if (item != NULL)
+	{
+		item->data = data;
+		item->next = NULL;
+		item->prev = list->tail;
+		if (list->head == NULL)
+		{
+			list->head = item;
+			list->tail = item;
+		}
+		else
+		{	
+			list->tail->next = item;
+			list->tail = item;
+			
+		}
+	}
+	else exit(0);
+}
+
+void PrintList(struct Node* list)
+{
+	while (list)
+	{
+		printf("%d", list->data);
+		list = list->next;
+		printf(" ");
+	}
+}
+
+void Reverse(struct Node* list, struct ListItem* newlist)
+{
+	while (list)
+	{
+		AddList(newlist, list->data);
+		list = list->prev;
+	}
+}
+
+int main()
+{
+	srand(time(NULL));
+	struct ListItem* list = (struct ListItem*)malloc(sizeof(struct ListItem));
+	if (list)
+	{
+		ListItemZ(list);
+		int counter;
+		printf("");
+		counter = VerificationI();
+		for (int i = 0; i < counter; i++)
+		{
+			AddList(list, rand()%100);
+		}
+
+		PrintList(list->head);
+		printf("\n");
+		struct ListItem* newlist = (struct ListItem*)malloc(sizeof(struct ListItem));
+		ListItemZ(newlist);
+		if (newlist != NULL)
+		{
+			Reverse(list->tail, newlist);
+			PrintList(newlist->head);
+		}
+	}
+	return 0;
+}
