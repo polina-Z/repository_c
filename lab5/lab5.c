d09beb6b259326c219996c496c8618bf9ac2901b
polina-Z <61199331+polina-Z@users.noreply.github.com>
2020-04-07 16:48:57 +0300

    Update lab5.c

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -25,9 +25,9 @@ int VerificationI()
 	while (1)
 	{
 		rewind(stdin);
-		if (scanf_s("%d%c", &choice, &term1, 102) != 2 || term1 != '\n' || choice <= 0 || choice > MAX)
+		if (scanf("%d%c", &choice, &term1) != 2 || term1 != '\n' || choice <= 0 || choice > MAX)
 		{
-			printf("Неправильный ввод.\n");
+			printf("ГЌГҐГЇГ°Г ГўГЁГ«ГјГ­Г»Г© ГўГўГ®Г¤.\n");
 		}
 		else
 		{
